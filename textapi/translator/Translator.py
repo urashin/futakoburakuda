26fdd03d267f827db21a50d6bd0a0ec1357c515b
takahiro kobori <ikkeykokoro5470@gmail.com>
2019-12-15 05:06:40 +0900

    SentenceCreatorのパラメータ調整

--- textapi/translator/Translator.py
+++ textapi/translator/Translator.py
@@ -27,15 +27,28 @@ class Translator:
         ランダムで１つ返す
         """
         return random.choice([v for k, v in sentence_dic.items()])
+    
+    ###
+    # For Test
+    ###
+    def translate_with_candidates(self, post, type):
+        post = self.preprocess.exec(post)
+        google_phrase = self.google_extractor.get_phrase(post)
+        sentence_dic = self.creater.create(google_phrase, type)
+        key_phrase = self.key_extractor.get_phrase(post)
+        sentence_dic.update(self.creater.create(key_phrase, type))
+    
+        return self.choiceOne(sentence_dic), sentence_dic
+
 
     def translate_file(self, filename, output, sep="\n"):
         with codecs.open(filename, "r", "utf-8") as f:
             posts = f.read().rstrip(sep).split(sep)
         o = codecs.open(output, "w", "utf-8")
 
-        for i, post in enumerate(posts, start=0):
+        for i, post in enumerate(posts[:30], start=0):
             print(i)
-            result_dic, results = self.translate(post, "jk")
+            result_dic, candidates = self.translate_with_candidates(post, "jk")
             # テキスト出力
             output = ""
             output += "sentence_id: {}\n".format(i)
@@ -43,7 +56,7 @@ class Translator:
             output += "--choiced--\n"
             output += "{}\n".format(result_dic)
             output += "--candidates--\n"
-            for k, v in results.items():
+            for k, v in candidates.items():
                 output += "method:{}:{}\n".format(k, v)
             o.write(output + "==========\n\n")
 

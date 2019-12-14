9e2276efc0cb1cfaa02ba892a4ba290fc75c0a9e
鈴木一正 <kazu@kzms.local>
2019-12-14 22:33:21 +0900

    fixes

--- imgapi/create_image.py
+++ imgapi/create_image.py
@@ -3,6 +3,7 @@
 
 from IPython.display import Image, display_jpeg
 import sys
+import os
 import re
 import numpy as np
 import cv2
@@ -11,19 +12,22 @@ import cv2
 class create_image:
 	def display_all(self):
 		display_jpeg(Image("blended.jpg"))
-	
+
 	def situ(self,objs):
 		if '室内' in objs:
 			return 'indoor'
 		elif '屋外' in objs:
 			return 'outdoor'
 		return 'unknown'
-	
+
 	def persons(self,objs):
-		num = 0
 		p = r'(.*)人'
 		pc = re.compile(p)
-		persons = {}
+		persons = {'num': '0'}
+		persons['smile'] = self.smile(objs)
+		persons['pose'] = self.standing(objs)
+		persons['closeup'] = self.up(objs)
+		persons['table'] = self.table(objs)
 		for obj in objs:
 			result = pc.match(obj)
 			if result:
@@ -31,38 +35,34 @@ class create_image:
 					persons['num'] = result.group(1)
 				else:
 					persons['num'] = '5' # 不明なので決めうちにする
-				persons['smile'] = self.smile(objs)
-				persons['pose'] = self.standing(objs)
-				persons['closeup'] = self.up(objs)
-				persons['table'] = self.table(objs)
 				return persons
 		return persons
-	
+
 	def smile(self,objs):
 		if 'スマイル' in objs:
 			return 'smile'
 		return ''
-	
+
 	def table(self,objs):
 		if 'テーブル' in objs:
 			return 'table'
 		return ''
-	
+
 	def up(self,objs):
 		if 'クローズアップ' in objs:
 			return 'closeup'
 		return ''
-	
+
 	def standing(self,objs):
 		for obj in objs:
 			if '座って' in obj:
 				return 'sitting'
 		return 'standing'
-	
+
 	def objects(self,objs):
 		countables = ['食べ物','画面', '犬']
 		middles = ['家', 'ビル', '木', '植物']
-		backgrounds = ['海', '山', '空']
+		backgrounds = ['海', '山', '空', '屋外', '室内']
 		objects = {'countables':[], 'middles':[], 'backgrounds':[]}
 		for obj in objs:
 			if  obj in countables:
@@ -71,11 +71,13 @@ class create_image:
 				objects['middles'].append(obj)
 			elif obj in backgrounds:
 				objects['backgrounds'].append(obj)
+		if len(objects['backgrounds']) == 0:
+			objects['backgrounds'].append('default')
 		return objects
 
 	def create_text_list(self,text):
 		return text.split('、')
-	
+
 	def analyze(self,text):
 		images = {}
 		objs = text.split('、')
@@ -83,18 +85,18 @@ class create_image:
 		images['persons'] = self.persons(objs)
 		images['objects'] = self.objects(objs)
 		return images
-	
+
 	def analyze_list(self,text_list):
 		images = {}
 		images['situation'] = self.situ(text_list)
 		images['persons'] = self.persons(text_list)
 		images['objects'] = self.objects(text_list)
 		return images
-	
+
 	def merge(self,base, img, x, y):
 		base[y:y+img.shape[0], x:x+img.shape[1]] = img
 		return base
-	
+
 	def get_masked(self,foreground, background):
 		# グレースケールに変換する。
 		gray = cv2.cvtColor(foreground, cv2.COLOR_BGR2GRAY)
@@ -110,61 +112,50 @@ class create_image:
 		roi = background[y : y + h, x : x + w, :]
 		masked_img = np.where(np.expand_dims(mask == 255, -1), foreground, roi)
 		return masked_img
-	
+
 	def draw_middles(self,objs, base):
 		return draw_images(objs, base, 0, 0)
-	
+
 	def draw_images(self,objs, base, x, y):
 		images = {}
 		for obj in objs:
 			images[obj] = cv2.imread(f"images/{obj}.png")
+			file = f"images/{obj}.png"
+			if not os.path.isfile(file):
+				print(f"{obj} not found\n")
+				return base
 			#images[obj] = cv2.cvtColor(images[obj], cv2.COLOR_BGR2RGB)
 			base = merge(base, images[obj], x, y)
 		return base
-	
+
 	def draw_backgrounds(self,objs):
-		base = cv2.imread("images/base.jpg") # とりあえずきめうち
+		base = cv2.imread(f"images/{objs[0]}.png") # とりあえずきめうち
+		#base = cv2.imread("images/base.jpg") # とりあえずきめうち
 		#base = cv2.cvtColor(base, cv2.COLOR_BGR2RGB)
 		return base
-	
+
 	def draw_persons(self,persons, base):
 		#  num, smile, pose, closeup, table
 		# 3_closeup_standing_smile_table_persons
+		if int(persons['num']) == 0:
+			return base;
 		if int(persons['num']) > 5:
 			persons['num'] = 5
 		img_name = f"images/{persons['num']}_{persons['closeup']}_{persons['pose']}_{persons['smile']}_{persons['table']}_persons.png"
 		print(img_name)
 		person_img = cv2.imread(img_name)
-	
+
 		masked = self.get_masked(person_img, base)
-		'''
-		# mask 処理 ここから
-		# グレースケールに変換する。
-		gray = cv2.cvtColor(person_img, cv2.COLOR_BGR2GRAY)
-		# 2値化する。
-		_, binary = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY_INV)
-		# 輪郭抽出する。
-		contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-		# 検出された輪郭内部を255で塗りつぶす。
-		mask = np.zeros_like(binary)
-		cv2.drawContours(mask, contours, -1, color=255, thickness=-1)
-		h, w = person_img.shape[:2]  # 前景画像の大きさ
-		x, y = 300, 300  # 背景画像の座標上で前景画像を貼り付ける位置
-		roi = base[y : y + h, x : x + w, :]
-		masked_img = np.where(np.expand_dims(mask == 255, -1), person_img, roi)
-		### mask処理ここまで
-		'''
-		#original way
 		base = self.merge(base, masked, 0, 0)
-	
+
 		return base
-	
+
 	def obaka_merge(self,objs,file_path):
 		images = {}
 		base = self.draw_backgrounds(objs['objects']['backgrounds'])
-	
+
 		base = self.draw_images(objs['objects']['middles'], base, 0, 0)
-		base = self.draw_images(objs['objects']['countables'], base, 0, 320)
+		base = self.draw_images(objs['objects']['countables'], base, 0, 0)
 		base = self.draw_persons(objs['persons'], base)
 		# blended = cv2.addWeighted(src1=base,alpha=0.7,src2=images[obj],beta=0.3,gamma=0)
 		cv2.imwrite(file_path, base)
@@ -192,4 +183,3 @@ if __name__ == "__main__":
 	crt_img = create_image()
 	text_list = crt_img.create_text_list(args[1])
 	crt_img.create(text_list,file_path)
-
